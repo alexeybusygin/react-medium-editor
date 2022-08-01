a503e92b7b332c3500f415342a25e5fcc058928d
Alexey Busygin <alexey@busygin.com>
2022-08-01 19:52:16 +0500

    React 16.14

---
+++ dist/index.cjs.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var editor_js = _interopDefault(require('./editor.js'));
+
+
+
+module.exports = editor_js;
